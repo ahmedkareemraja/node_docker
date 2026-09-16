b979d85d6d05c4a299ded676849cc416ec5f9b50
ahmedkareemraja <ahmedkareemraja123@gmail.com>
2026-09-17 00:31:30 +0500

    First commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
+
+
+const PORT = process.env.PORT;
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
