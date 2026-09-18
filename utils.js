74629e85f247adbc9f5f2149df288b796525576f
ahmedkareemraja <ahmedkareemraja123@gmail.com>
2026-09-19 00:32:26 +0500

    v1- CI pipeline

---
+++ utils.js
@@ -0,0 +1,11 @@
+
+// Random test functions created to run tests in Jest
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+module.exports = { add, subtract };
